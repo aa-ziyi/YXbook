3054dfd1d4bda8ce36bbcaa432b8aad9c81b0fa9
ziyi <ziyi931217@163.com>
2019-07-29 08:59:15 +0800

    add login

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-// next.config.js
+/* eslint-disable */
+const withCss = require('@zeit/next-css');
 const withTypescript = require('@zeit/next-typescript');
-// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-/**
- * next的配置文件，支持配置嵌套
- */
-module.exports = withTypescript({
-  webpack(config, options) {
-    //  这里面还可以再配置哦 最后一个要return
-    // if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
-    return config;
-  }
-});
+
+// fix: prevents error when .css files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.css'] = file => {};
+}
+
+module.exports = withCss(withTypescript());
--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import { InputItem, Button } from 'antd-mobile';
+import { createForm } from 'rc-form';
 
-export default class Helps extends React.Component {
-  constructor(props) {
-    super(props);
+interface Props {
+  form: any;
+}
+
+class BasicInputExample extends React.Component<Props> {
+  handleLogin() {
+    console.log('click');
   }
   render() {
-    return <div>login</div>;
+    const { getFieldProps } = this.props.form;
+    return (
+      <div>
+        <InputItem
+          {...getFieldProps('autofocus')}
+          clear
+          placeholder="auto focus"
+        >
+          用户名
+        </InputItem>
+        <InputItem
+          {...getFieldProps('focus')}
+          clear
+          placeholder="click the button below to focus"
+        >
+          密码
+        </InputItem>
+        <Button
+          type="primary"
+          onClick={() => {
+            this.handleLogin();
+          }}
+        >
+          primary
+        </Button>
+      </div>
+    );
   }
 }
+
+export default createForm()(BasicInputExample);
