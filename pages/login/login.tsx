3c48a78a84518f76892eb0210492615a34eb89e0
ziyi <ziyi931217@163.com>
2019-07-30 08:24:50 +0800

    feat:登陆逻辑

--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { InputItem, Button } from 'antd-mobile';
+import { InputItem, Button, WhiteSpace, WingBlank } from 'antd-mobile';
 import { createForm } from 'rc-form';
+import instance from '../../utils/axios';
 
 interface Props {
   form: any;
@@ -9,33 +10,52 @@ interface Props {
 class BasicInputExample extends React.Component<Props> {
   handleLogin() {
     console.log('click');
+    this.props.form.validateFields((error, value) => {
+      console.log(error, value);
+      if (!error) {
+        instance({
+          url: '/user',
+          method: 'post',
+          data: { id: 1 }
+        });
+      }
+    });
   }
   render() {
-    const { getFieldProps } = this.props.form;
+    const { getFieldProps, getFieldError } = this.props.form;
     return (
       <div>
         <InputItem
-          {...getFieldProps('autofocus')}
+          {...getFieldProps('name', {
+            rules: [{ required: true }]
+          })}
           clear
-          placeholder="auto focus"
+          placeholder="请输入用户名"
+          error={getFieldError('name') || [].length}
         >
           用户名
         </InputItem>
         <InputItem
-          {...getFieldProps('focus')}
+          {...getFieldProps('password', {
+            rules: [{ required: true, message: '请输入密码' }]
+          })}
           clear
-          placeholder="click the button below to focus"
+          placeholder="请输入密码"
+          error={getFieldError('password') || [].length}
         >
           密码
         </InputItem>
-        <Button
-          type="primary"
-          onClick={() => {
-            this.handleLogin();
-          }}
-        >
-          primary
-        </Button>
+        <WhiteSpace />
+        <WingBlank>
+          <Button
+            type="primary"
+            onClick={() => {
+              this.handleLogin();
+            }}
+          >
+            登陆
+          </Button>
+        </WingBlank>
       </div>
     );
   }
